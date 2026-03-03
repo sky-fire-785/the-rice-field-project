9c70ed92af6b10657f94f1e2e16829036b140ce1
sky-fire-785 <jackmgearhart@gmail.com>
2026-03-02 22:53:32 -0800

    Update install.sh
    dev0.01
    made some new code and fixed some bugs

--- install.sh
+++ install.sh
@@ -6,10 +6,14 @@
 #or via fetch:  
 #    sh -c "$(fetch -o - https://raw.githubusercontent.com/sky-fire-785/Project-Aphrodite/refs/heads/Main-PC/install.sh)"
 #
+# You can install the sorce code by runing: 
+# wget https://raw.githubusercontent.com/sky-fire-785/Project-Aphrodite/refs/heads/Main-PC/install.sh ; sh install.sh
+#
 #this project is made for arch linux kde plasma 
 #remove all ## for full functionality
 
-ansi_art='                 
+ansi_art='    
+    project
     ___         __                    ___ __      
    /   | ____  / /_  _________  ____/ (_) /____ 
   / /| |/ __ \/ __ \/ ___/ __ \/ __  / / __/ _ \
@@ -18,14 +22,15 @@ ansi_art='
       /_/                                       '
 
 clear
-echo "project"
 echo -e "\n$ansi_art\n"
 echo By Jack Gearhart
 #inspierd by omarchy
-#this uses a lightly edited verson of the zsh install to make the code run smother line: 38
+
+#starts the loop
+while true; do
 
 #asks if you would like to continue
-echo "select anb install option"
+echo "select an install option"
 echo "1) install" 
 echo "2) update"
 echo "3) remove"
@@ -39,64 +44,62 @@ case $choice in
 echo "installing..."
 
 #install all needed things
-sudo pacman -Syu --needed git kitty zsh fastfetch wget fuse ly extra/ttf-noto-nerd base-devel vivaldi zig --noconferm
-cd; git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si; cd
+echo "Updating system & installing essential tools..."
+sudo pacman -Syu --needed git kitty zsh fastfetch wget fuse ly extra/ttf-noto-nerd base-devel vivaldi zig curl --noconfirm
+cd; git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si ; cd
 yay -S walker-bin elephant-bin --noconfirm
 #install vscode needed to do
 
-sh -c "$(wget -qO- https://raw.githubusercontent.com/sky-fire-785/Project-Aphrodite/refs/heads/Main-PC/zsh%20config/zsh_install.sh)" ;\ 
- git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ;\
- git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ;\ 
- git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k 
+RUNZSH=no sh -c "$(wget -qO- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" ;\ 
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ;\
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ;\ 
+git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k 
 
 #fix this
-cd .local/bin ; wget https://raw.githubusercontent.com/sky-fire-785/Project-Aphrodite/refs/heads/Main-PC/zsh%20config/env 
-\; cd
+cd ~/.local/bin ; wget https://raw.githubusercontent.com/sky-fire-785/Project-Aphrodite/refs/heads/Main-PC/zsh%20config/env
 
 #makes config file for zsh and pk10
- rm -rf .zshrc
+mv .zshrc .zshrc_384
 wget https://raw.githubusercontent.com/sky-fire-785/Project-Aphrodite/refs/heads/Main-PC/zsh%20config/.zshrc \
  https://raw.githubusercontent.com/sky-fire-785/Project-Aphrodite/refs/heads/Main-PC/pk10/.p10k.zsh 
 
 #enter the config derectory
-cd && cd .config
+cd ~/.config
 
 #kitty config
 mkdir kitty
 cd kitty
 wget https://raw.githubusercontent.com/sky-fire-785/Project-Aphrodite/refs/heads/Main-PC/kitty/current-theme.conf \
  https://raw.githubusercontent.com/sky-fire-785/Project-Aphrodite/refs/heads/Main-PC/kitty/kitty.conf
- cd && cd .config
+cd ~/.config
 
 #makes a fastfetch derectory
 mkdir fastfetch
 cd fastfetch
 wget https://raw.githubusercontent.com/sky-fire-785/Project-Aphrodite/refs/heads/Main-PC/FastFetch/arch.txt\
  https://raw.githubusercontent.com/sky-fire-785/Project-Aphrodite/refs/heads/Main-PC/FastFetch/config.jsonc
-cd && cd .config
+cd ~/.config
 
 #make the walker derectory
 mkdir walker
 cd walker
 wget https://raw.githubusercontent.com/sky-fire-785/project-aphrodite/6d714d75f519e17ecceb1d9659e42b94ac767dbc/walker/config.toml
-mkdir themes && cd themes && mkdir my-themes && cd my-themes 
+mkdir -P themes/my-themes ; cd my-themes 
 wget https://raw.githubusercontent.com/sky-fire-785/project-aphrodite/6d714d75f519e17ecceb1d9659e42b94ac767dbc/walker/themes/my-theme/layout.xml \
  https://raw.githubusercontent.com/sky-fire-785/project-aphrodite/6d714d75f519e17ecceb1d9659e42b94ac767dbc/walker/themes/my-theme/style.css
-cd ; cd .config
+cd ~/.config
 
 #edit the pacman.conf
 cd /etc
-sudo rm -rf pacman.conf
-sudo wget https://raw.githubusercontent.com/sky-fire-785/project-aphrodite/refs/heads/Main-PC/pacman/pacman.conf
-cd
+sudo mv pacman.conf .old_pacman.conf #need to make better
+sudo wget pacman.conf https://raw.githubusercontent.com/sky-fire-785/project-aphrodite/refs/heads/Main-PC/pacman/pacman.conf
 
 #makes a new kde theme
-cd .local/share
+cd ~/.local/share
 sudo mkdir -p plasma/look-and-feel/skys/contents/layouts ; cd plasma/look-and-feel/skys
 sudo wget https://raw.githubusercontent.com/sky-fire-785/Project-Aphrodite/refs/heads/Main-PC/kde%20config/metadata.json
 sudo cd contents ; wget https://raw.githubusercontent.com/sky-fire-785/Project-Aphrodite/refs/heads/Main-PC/kde%20config/contents/defaults
 sudo cd layouts ; wget https://raw.githubusercontent.com/sky-fire-785/Project-Aphrodite/refs/heads/Main-PC/kde%20config/contents/layouts/org.kde.plasma.desktop-layout.js
-cd
 
 #sets defalts 
 sudo systemctl disable sddm.service
@@ -107,21 +110,36 @@ echo "Install Complete"
 2)
 echo "Updating..."
 echo "This needs to be built"
-#code go here
+
+##sudo pacman -Syu --noconfirm
+
+#updates zsh
+curl --progress-bar -L -z ~/.zshrc -o ~/.zshrc "https://raw.githubusercontent.com/sky-fire-785/Project-Aphrodite/refs/heads/Main-PC/zsh%20config/.zshrc"
+curl --progress-bar -L -z ~/.p10k.zsh -o ~/.p10k.zsh "https://raw.githubusercontent.com/sky-fire-785/Project-Aphrodite/refs/heads/Main-PC/pk10/.p10k.zsh"
+
+#updates fastfetch
+cd ~/.config/fastfetch
+curl --progress-bar -L -z ~/.config/fastfetch/arch.txt -o ~/fastfetch/arch.txt https://raw.githubusercontent.com/sky-fire-785/Project-Aphrodite/refs/heads/Main-PC/FastFetch/arch.txt
+curl --progress-bar -L -z ~/.config/fastfetch/config.jsonc -o ~/fastfetch/config.jsonc https://raw.githubusercontent.com/sky-fire-785/Project-Aphrodite/refs/heads/Main-PC/FastFetch/config.jsonc
+
+##brake #this means this is the end of the line of code
 ;;
 
 3)
 echo "Removeing..."
 echo "This needs to be built"
-#code go here
+#code go here 
+##brake #this means this is the end of the line of code
 ;;
 
 4)
 echo "Exiting script..."
 exit 0
+##brake #this means this is the end of the line of code
 ;;
 
 *)
 echo "Invalid option. Please pick 1, 2, 3, or 4."
-;;
+;; #do not put a brake here
 esac
+done
